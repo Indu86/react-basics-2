b27487045a5ff8ca533efec559e82bf0278d19df
indu86 <induraghu1986@gmail.com>
2023-05-16 11:48:58 +0100

    components created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import Component1 from './Component1'
+
+export default function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Header/>
+      <Footer/>
+      <Component1/>
+    </div>
+  )
+}
