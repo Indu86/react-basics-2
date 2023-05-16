dd02efe8108798255401ce08334169c7544cd547
indu86 <induraghu1986@gmail.com>
2023-05-16 15:54:22 +0100

    props learned

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import Component1 from './Component1'
+
 
 export default function App() {
   return (
     <div>
       <h1>App</h1>
-      <Header/>
-      <Footer/>
-      <Component1/>
+      <h1>Component1</h1>
+      <header/> <h1>Component1</h1>
+      <h1>Component1</h1>
+      <Header name={'Elon musk'} age={41} ceoBoollean={false} />
+      <Footer somepropdata={'my data'}/>
     </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Footer from './Footer'
 
-export default function Header() {
+export default function Header({name, age,}) {
   return (
     <div>
-      <h1>Header</h1>
+      <h1>welcome, {name}, age={age}, </h1>
       <Footer/>
     </div>
   )
-}
+}   
